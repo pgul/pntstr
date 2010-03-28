68462e82e219f23142aa1f50e037b2226942c5e3
Stas Degteff <grumbler@grumbler.org>
2010-03-28 18:54:09 +0000

    Add comment about limitation

--- pntstr.pl
+++ pntstr.pl
@@ -2,6 +2,8 @@
 
 # Point string processor
 # (c) Stas Degteff 2:5080/102
+#
+# Only "Boss+Point" pointlist format is supported
 
 # $Id$
 #
@@ -24,6 +26,9 @@ HELP
 #-----------------------------------------------------------------------------#
 #
 # $Log$
+# Revision 1.5  2010/03/28 18:54:09  stas_degteff
+# Add comment about limitation
+#
 # Revision 1.4  2002/12/14 16:21:20  stas_degteff
 # Use full point address
 #
